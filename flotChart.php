1f789ff79b8321f83b6ebd1587aae06d8f5098f5
mquade <mail@technologiewerk-qua.de>
2016-09-30 17:06:49 +0200

    Signed-off-by: mquade <mail@technologiewerk-qua.de>

--- flotChart.php
+++ flotChart.php
@@ -15,7 +15,7 @@
 <?php
 require 'dbConnect.php';
 
-$query = "SELECT id, value FROM agriot_productive.`values` JOIN sensors ON values.sensorId=sensors.id JOIN users ON sensors.ownerId = users.id WHERE users.id = :id AND values.timestamp > '2016-09-01 03:25:43' ORDER BY values.timestamp";
+$query = "SELECT values.id, value FROM agriot_productive.`values` JOIN sensors ON values.sensorId=sensors.id JOIN users ON sensors.ownerId = users.id WHERE users.id = :id AND values.timestamp > '2016-09-01 03:25:43' ORDER BY values.timestamp";
 $statement = $db->prepare ( $query );
 $statement->bindParam ( ':id', $_GET ["id"] );
 $statement->execute ();
