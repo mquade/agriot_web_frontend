a6170be190c0598df9bf21deea9c20ac9bc02464
mquade <mail@technologiewerk-qua.de>
2016-09-30 18:46:11 +0200

    Signed-off-by: mquade <mail@technologiewerk-qua.de>

--- bower_components/gentelella/production/login.php
+++ bower_components/gentelella/production/login.php
@@ -5,14 +5,16 @@ require '../../../dbConnect.php';
 echo "foo";
 if(isset($_GET['login'])) {
 	$email = $_POST['email'];
-	$passwort = $_POST['passwort'];
+	$password = $_POST['password'];
+	echo $email . " " . $password;
+
 	
 	$statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
 	$result = $statement->execute(array('email' => $email));
 	$user = $statement->fetch();
 		
 	//Überprüfung des Passworts
-	if ($user !== false && password_verify($passwort, $user['passwordHashed'])) {
+	if ($user !== false && password_verify($password, $user['passwordHashed'])) {
 		$_SESSION['userid'] = $user['id'];
 		die('Login erfolgreich. Weiter zu <a href="geheim.php">internen Bereich</a>');
 	} else {
