5f85ed3c34bd0d97d79cdfdf80df45e9f8a677ca
mquade <mail@technologiewerk-qua.de>
2016-09-30 16:20:32 +0200

    Signed-off-by: mquade <mail@technologiewerk-qua.de>

--- dbTest.php
+++ dbTest.php
@@ -3,9 +3,7 @@ require 'dbConnect.php';
 
 $query = 'SELECT * FROM agriot_productive.users WHERE id <= :id';
 $statement = $db->prepare ( $query );
-$statement->bindParam ( ':id', $id );
-
-$id = 3;
+$statement->bindParam ( ':id', $_GET ["id"] );
 
 $statement->execute ();
 
@@ -17,6 +15,16 @@ print_r ( $allResults );
 
 echo $allResults [0] ['firstName'];
 
+print "</pre>";
+
+$query = "SELECT value, timestamp FROM agriot_productive.`values` JOIN sensors ON values.sensorId=sensors.id JOIN users ON sensors.ownerId = users.id WHERE users.id = :id AND values.timestamp > '2016-09-01 03:25:43'";
+$statement = $db->prepare ( $query );
+$statement->bindParam ( ':id', $_GET ["id"] );
+$statement->execute ();
+$allResults = $statement->fetchAll ( PDO::FETCH_ASSOC );
+
+print "<pre>";
+print_r ( $allResults );
 print "</pre>";
 
 // $db->prepare("SELECT * FROM agriot_productive.users WHERE id = :id");
