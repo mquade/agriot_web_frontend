1d798c60b78e5f439268642add21b0671f137d31
mquade <mail@technologiewerk-qua.de>
2016-09-30 15:22:53 +0200

    Signed-off-by: mquade <mail@technologiewerk-qua.de>

--- dbTest.php
+++ dbTest.php
@@ -2,27 +2,30 @@
 require 'dbConnect.php';
 
 $query = 'SELECT * FROM agriot_productive.users WHERE id = :id';
-$statement = $db->prepare($query);
-$statement ->bindColumn(':id', 3);
-$statement ->execute();
+$statement = $db->prepare ( $query );
+$statement ->bindParam(':id', $id);
 
-echo $statement[2];
+$id = 3;
 
-//$db->prepare("SELECT * FROM agriot_productive.users WHERE id = :id");
-//$db->exec(array(":id" => $_GET["id"]));
+$statement->execute ();
 
-//echo 
+echo $statement [2];
+
+// $db->prepare("SELECT * FROM agriot_productive.users WHERE id = :id");
+// $db->exec(array(":id" => $_GET["id"]));
+
+// echo
 /*
-
-
-
-$sql = "SELECT * FROM agriot_productive.users";
-$result = $db->query ( $sql );
-echo "<ul>";
-foreach ( $result as $row ) {
-	echo "<li>" . htmlspecialchars ( $row ["id"] ) . ": " . htmlspecialchars ( $row ["firstName"] ) . "</li>";
-}
-echo "</ul>";
-
-*/
+ *
+ *
+ *
+ * $sql = "SELECT * FROM agriot_productive.users";
+ * $result = $db->query ( $sql );
+ * echo "<ul>";
+ * foreach ( $result as $row ) {
+ * echo "<li>" . htmlspecialchars ( $row ["id"] ) . ": " . htmlspecialchars ( $row ["firstName"] ) . "</li>";
+ * }
+ * echo "</ul>";
+ *
+ */
 ?>
